d3d82be593f202c368b4ab3710e3f7d4050d6bbb
Phi <orjan.roren@gmail.com>
2023-03-28 10:14:11 +0200

    Add lruCache struct
    Add lruCache struct

--- storage/sealer/piece_reader.go
+++ storage/sealer/piece_reader.go
@@ -2,7 +2,9 @@ package sealer
 
 import (
 	"bufio"
+	"container/list"
 	"context"
+	"errors"
 	"io"
 	"sync"
 
@@ -21,14 +23,11 @@ import (
 var MaxPieceReaderBurnBytes int64 = 1 << 20 // 1M
 var ReadBuf = 128 * (127 * 8)               // unpadded(128k)
 
-type pieceGetter func(ctx context.Context, offset uint64) (io.ReadCloser, error)
-
 type pieceReader struct {
-	ctx       context.Context
-	getReader pieceGetter
-	pieceCid  cid.Cid
-	len       abi.UnpaddedPieceSize
-	onClose   context.CancelFunc
+	ctx      context.Context
+	pieceCid cid.Cid
+	len      abi.UnpaddedPieceSize
+	onClose  context.CancelFunc
 
 	closed bool
 	seqAt  int64 // next byte to be read by io.Reader
@@ -38,6 +37,68 @@ type pieceReader struct {
 	lru  *lruCache
 }
 
+type lruCache struct {
+	mu       sync.Mutex
+	cache    map[uint64]*list.Element
+	lruList  *list.List
+	size     int
+	getValue func(uint64) (io.ReadCloser, error)
+}
+
+type lruCacheEntry struct {
+	offset uint64
+	reader io.ReadCloser
+}
+
+func newLRUCache(size int) *lruCache {
+	return &lruCache{
+		size:    size,
+		cache:   make(map[uint64]*list.Element),
+		lruList: list.New(),
+	}
+}
+
+func (c *lruCache) Get(offset uint64) (io.ReadCloser, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if el, ok := c.cache[offset]; ok {
+		c.lruList.MoveToFront(el)
+		return el.Value.(*lruCacheEntry).reader, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) add(offset uint64, reader io.ReadCloser) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.size == 0 {
+		return
+	}
+
+	if len(c.cache) >= c.size {
+		lastEl := c.lruList.Back()
+		delete(c.cache, lastEl.Value.(*lruCacheEntry).offset)
+		c.lruList.Remove(lastEl)
+	}
+
+	el := c.lruList.PushFront(&lruCacheEntry{
+		offset: offset,
+		reader: reader,
+	})
+	c.cache[offset] = el
+}
+
+func (c *lruCache) touch(offset uint64) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if el, ok := c.cache[offset]; ok {
+		c.lruList.MoveToFront(el)
+	}
+}
+
 func (p *pieceReader) init() (_ *pieceReader, err error) {
 	stats.Record(p.ctx, metrics.DagStorePRInitCount.M(1))
 
@@ -45,7 +106,15 @@ func (p *pieceReader) init() (_ *pieceReader, err error) {
 	p.pool = &sync.Pool{}
 	p.lru = newLRUCache(5) // Adjust the cache size based on your requirements
 	p.pool.New = func() interface{} {
-		return p.getReader(uint64(p.seqAt))
+		reader, err := p.getReader(p.ctx, uint64(p.seqAt))
+		if err != nil {
+			log.Errorw("error creating reader for pool",
+				"pieceCid", p.pieceCid,
+				"offset", p.seqAt,
+				"error", err)
+			return nil
+		}
+		return reader
 	}
 
 	return p, nil
@@ -67,14 +136,15 @@ func (p *pieceReader) Close() error {
 		return err
 	}
 
-	if p.r != nil {
-		if err := p.r.Close(); err != nil {
-			return err
+	// Close and release any acquired readers
+	for el := p.lru.lruList.Front(); el != nil; el = el.Next() {
+		if el.Value != nil {
+			if reader, ok := el.Value.(*lruCacheEntry); ok && reader.reader != nil {
+				if err := reader.reader.Close(); err != nil {
+					return err
+				}
+			}
 		}
-		if err := p.r.Close(); err != nil {
-			return err
-		}
-		p.r = nil
 	}
 
 	p.onClose()
@@ -134,15 +204,23 @@ func (p *pieceReader) readAtUnlocked(b []byte, off int64) (n int, err error) {
 	stats.Record(p.ctx, metrics.DagStorePRBytesRequested.M(int64(len(b))))
 
 	// Check if we have an existing reader in the LRU cache
-	reader, ok := p.lru.get(off)
+	reader, ok := p.lru.Get(uint64(off))
 	if !ok {
 		// Acquire a reader from the pool or create a new one if necessary
 		reader = p.pool.Get().(io.ReadCloser)
-		_, err = reader.Seek(off, io.SeekStart)
+
+		// Type assert the reader to an io.Seeker
+		seeker, ok := reader.(io.Seeker)
+		if !ok {
+			return 0, errors.New("reader does not implement io.Seeker")
+		}
+
+		// Use the seeker to perform the Seek operation
+		_, err = seeker.Seek(off, io.SeekStart)
 		if err != nil {
 			return 0, err
 		}
-		p.lru.add(off, reader)
+		p.lru.add(uint64(off), reader)
 	}
 
 	// Create a bufio.Reader to buffer the read
@@ -157,7 +235,7 @@ func (p *pieceReader) readAtUnlocked(b []byte, off int64) (n int, err error) {
 	}
 
 	// Update the LRU cache and return the reader to the pool
-	p.lru.touch(off)
+	p.lru.touch(uint64(off))
 	p.pool.Put(reader)
 
 	return n, err
